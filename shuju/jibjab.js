23e8c5c3d01b61171ab16fdae2d997904b4dd3e6
Chibinl <74690915+Chibinl@users.noreply.github.com>
2021-02-10 16:53:59 +0800

    Update jibjab.js

--- shuju/jibjab.js
+++ shuju/jibjab.js
@@ -2,15 +2,15 @@
 JibJab unlock
 
 Surge4:
-http-response https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/jibjab.js
+http-response https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Chibinl/vip/main/shuju/jibjab.js
 
 QX：
-https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/jibjab.js
+https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user url script-response-body https://raw.githubusercontent.com/Chibinl/vip/main/shuju/jibjab.js
 
 Surge & QX MITM = origin-prod-phoenix.jibjab.com
 */
 
 let obj = JSON.parse($response.body);
 obj.data.attributes["is-paid"] = true;
-obj.data.attributes["email"] = "禁止牟利,TG频道@NobyDa";
+obj.data.attributes["email"] = "@Chipun";
 $done({body: JSON.stringify(obj)});

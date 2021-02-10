064c48096124bf1de4123183d448c24fdbfebb44
Chibinl <74690915+Chibinl@users.noreply.github.com>
2021-02-10 16:33:55 +0800

    Update xxys.js

--- shuju/xxys.js
+++ shuju/xxys.js
@@ -1,9 +1,9 @@
 /*
 XiaoXiaoYingShi unlock Vip
 QX:
-https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xxys.js
+https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/Chibinl/vip/main/shuju/xxys.js
 Surge:
-http-response https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xxys.js
+http-response https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Chibinl/vip/main/shuju/xxys.js
 
 MITM = *.xiaoxiao*.com
 */

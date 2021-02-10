6b500948d82f5a9f5e7ef98191434f2425dc1732
Chibinl <74690915+Chibinl@users.noreply.github.com>
2021-02-10 16:35:22 +0800

    Update aimeiju.js

--- shuju/aimeiju.js
+++ shuju/aimeiju.js
@@ -1,11 +1,11 @@
 /*
 爱美剧 unlock vip（官网：xmj.app）
-Made by huihui
+Made by Chipun
 QX : 
-^https?:\/\/api.bjxkhc.com\/index\.php\/app\/ios\/(vod\/show|(user|vod|topic|type)\/index) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/aimeiju.js
+^https?:\/\/api.bjxkhc.com\/index\.php\/app\/ios\/(vod\/show|(user|vod|topic|type)\/index) url script-response-body https://raw.githubusercontent.com/Chibinl/vip/main/shuju/aimeiju.js
 
 Surge4：
-http-response ^https?:\/\/api.bjxkhc.com\/index\.php\/app\/ios\/(vod\/show|(user|vod|topic|type)\/index) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/aimeiju.js
+http-response ^https?:\/\/api.bjxkhc.com\/index\.php\/app\/ios\/(vod\/show|(user|vod|topic|type)\/index) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Chibinl/vip/main/shuju/aimeiju.js
 
 hostname = api.bjxkhc.com
 
